110cdf9b843036034081cf5b8d2c14fb72e93e63
Dan2759 <kwizeradan.kd27@gmail.com>
2023-10-18 15:40:37 -0700

    push.c

---
+++ push.c
@@ -0,0 +1,54 @@
+#include "monty.h"
+#include <stdbool.h>
+
+/**
+ * push - Pushes an element onto the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in the Monty bytecode file.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg = strtok(NULL, " \n");
+	int n = atoi(arg);
+
+	if (!arg || !is_valid_integer(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
+
+/**
+ * pall - Prints all values on the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in the Monty bytecode file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)line_number;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
