3c125e6efe8c3486f1e28b296632286678c38d78
MICHAEL <michaelogada104@gmail.com>
2023-10-19 20:21:21 +0000

    monty

--- push.c
+++ push.c
@@ -1,60 +1,37 @@
 #include "monty.h"
-#include <stdbool.h>
-
 /**
- * push - Pushes an element onto the stack.
- * @stack: A pointer to the top of the stack.
- * @line_number: The current line number in the Monty bytecode file.
- */
-void push(stack_t **stack, unsigned int line_number)
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_push(stack_t **head, unsigned int counter)
 {
-	char *arg = strtok(NULL, " \n");
-	int n = atoi(arg);
+	int n, j = 0, flag = 0;
 
-	if (!arg)
+	if (bus.arg)
 	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-
-	if (!is_valid_integer(arg))
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	stack_t *new_node = malloc(sizeof(stack_t));
-
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	if (*stack)
-		(*stack)->prev = new_node;
-	*stack = new_node;
-}
-
-/**
- * pall - Prints all values on the stack.
- * @stack: A pointer to the top of the stack.
- * @line_number: The current line number in the Monty bytecode file.
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
-
-	(void)line_number;
-
-	while (current)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
